9206dedbdf44a5650676b29600e564840a2c2dd6
One <One@DESKTOP-MIB22JC>
2022-04-25 22:15:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите то, что хотите донести до всех: ");
+        string userMessage = Console.ReadLine();
+        Console.Write("Сколько раз стоит об этом сообщить? :  ");
+        int quantityRepeat = Convert.ToInt32(Console.ReadLine());
+
+        while (quantityRepeat-- > 0)
+        {
+            Console.WriteLine(userMessage);
+        }
+    }
+}
